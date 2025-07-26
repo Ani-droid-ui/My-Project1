a6574eae4dc7e7c863c3b8657b14db319fad2b21
Ani-droid-ui <24yasina@dartfordgrammargirls.org.uk>
2025-07-26 17:45:29 +0100

    Random Number Picker
    This random number picker made with simple Python can be used to choose a random set of numbers. This simple program uses the library *random* and variables.

---
+++ main.py
@@ -0,0 +1,16 @@
+
+import random
+Big = (random.randrange(1,100))
+Medium1 = (random.randrange(1,100))
+Medium2 = (random.randrange(1,100))
+Medium3 = (random.randrange(1,100))
+Small1 = (random.randrange(1,100))
+Small2 = (random.randrange(1,100))
+Small3 = (random.randrange(1,100))
+Small4 = (random.randrange(1,100))
+Small5 = (random.randrange(1,100))
+
+print("The big prize is for number",Big)
+print("Medium prizes are for numbers",Medium1,Medium2,Medium3)
+print("The small prizes are for",Small1,Small2,Small3,Small4,Small5)
+
